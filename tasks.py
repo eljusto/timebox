a875c8828e0cb6da03b8bca92c15e7e1e55f84a9
Boris Zhidkov <eljusto@yandex-team.ru>
2022-05-23 19:03:13 +0300

    second part of refactoring

--- tasks.py
+++ tasks.py
@@ -9,13 +9,7 @@ class Task:
     title: str = ''
     url: str = ''
 
-def get_things_today_tasks_raw():
-    DB = os.path.expanduser(
-        "~/Library/Group Containers/JLMPQHK86H.com.culturedcode.ThingsMac/Things Database.thingsdatabase/main.sqlite"
-    )
-    return things.today(filepath=DB)
-
-def get_things_today_tasks():
+def _get_today_tasks():
     DB = os.path.expanduser(
         "~/Library/Group Containers/JLMPQHK86H.com.culturedcode.ThingsMac/Things Database.thingsdatabase/main.sqlite"
     )
@@ -26,21 +20,15 @@ def get_things_today_tasks():
         mytasks.append((duration_tag, task['title'], things.link(task['uuid'])))
     return mytasks
 
-def process_tasks(list_of_tasks):
+def get_things_today_tasks():
+    list_of_tasks = _get_today_tasks()
     processed_tasks = {}
 
     for task_tuple in list_of_tasks:
-        if task_tuple[0][-3:] == "min":
-            processed_tasks[task_tuple[1]] = Task(
-                minutes = int(task_tuple[0][:-3]),
+        processed_tasks[task_tuple[1]] = Task(
+                minutes = int(task_tuple[0][:-3]) if task_tuple[0][-3:] == "min" else 0,
                 title = task_tuple[1],
                 url = task_tuple[2]
-            )
-        else:
-            processed_tasks[task_tuple[1]] = Task(
-                minutes = 25,
-                title = task_tuple[1],
-                url = task_tuple[2],
-            )
+           )
 
     return processed_tasks
--- timebox.py
+++ timebox.py
@@ -1,10 +1,11 @@
+
 # %%
-import rumps
-import time
-import subprocess
-import shlex
-import os
 from pathlib import Path
+import os
+import rumps
+import shlex
+import subprocess
+import time
 
 import tasks
 from tasks import Task
@@ -12,15 +13,12 @@ from tasks import Task
 rumps.debug_mode(True)
 
 SEC_TO_MIN = 60
-
-
-# def timez():
-#     return time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.localtime())
-
-def timestamp():
-    return time.strftime("%Y-%m-%d %H:%M:%S.000", time.localtime())
+DEFAULT_MINUTES = 25
+LOG_FILE = str(Path.home() / "Downloads" / 'log.csv')
 
 # %%
+def timestamp():
+    return time.strftime("%Y-%m-%d %H:%M:%S.000", time.localtime())
 
 def hour_formatter(minutes):
     if minutes // 60 > 0:
@@ -42,15 +40,15 @@ class TimerApp(object):
         self.timer.count = 0
 
         self.app = rumps.App("Timebox", "⏱")
-        self.interval = SEC_TO_MIN
 
         self.current_task = Task()
 
         self.control_buttons = {}
         self.control_buttons['start_pause'] = rumps.MenuItem(
             title="Start Timer",
-            callback=lambda _: self.start_timer(_, self.interval),
+            callback=lambda _: self.on_press_start_pause(_, DEFAULT_MINUTES * SEC_TO_MIN),
             key="s",
+            template=True
         )
         self.control_buttons['stop'] = rumps.MenuItem(title="Stop Timer", callback=None, key="x")
         self.control_buttons['sync'] = rumps.MenuItem(
@@ -59,17 +57,10 @@ class TimerApp(object):
 
         self.buttons = {}
         self.buttons_callback = {}
-        for i in [5, 10, 15, 20, 25]:
+        for i in [1, 5, 10, 15, 20, 25]:
             task = Task(i, str(i) + " minutes", '')
 
-            def callback(_):
-                self.set_current_task(_, task)
-                for btn in [*self.things_buttons.values(), *self.buttons.values()]:
-                    btn.set_callback(None)
-                self.timer.count = 0
-                self.timer.end = self.interval
-                self.control_buttons['start_pause'].title = "Pause Timer"
-                self.timer.start()
+            callback = self.create_task_callback(task)
             self.buttons["btn_" + str(i)] = rumps.MenuItem(
                 title=task.title, callback=callback
             )
@@ -92,20 +83,14 @@ class TimerApp(object):
         ]
         self.sync_data()
 
-    def create_time_callback(self, task: Task):
+    def create_task_callback(self, task: Task):
         def inner_callback(_):
             self.set_current_task(_, task)
-            for btn in [*self.things_buttons.values(), *self.buttons.values()]:
-                btn.set_callback(None)
-            self.timer.count = 0
-            self.timer.end = self.interval
-            self.control_buttons['start_pause'].title = "Pause Timer"
-            self.timer.start()
+            self.restart_timer((task.minutes or DEFAULT_MINUTES) * SEC_TO_MIN)
         return inner_callback
 
     def sync_data(self):
-        self.things_tasks = tasks.get_things_today_tasks()
-        self.things_processed_tasks = tasks.process_tasks(self.things_tasks)
+        self.things_processed_tasks = tasks.get_things_today_tasks()
 
         self.sum_of_tasks_scheduled = sum(
             list(map(lambda x: x.minutes, self.things_processed_tasks.values()))
@@ -124,15 +109,18 @@ class TimerApp(object):
             # for idx, (title, (link, time)) in enumerate(self.things_processed_tasks.items())
         self.things_buttons = {}
         for idx, (title, task) in reversed(list(enumerate(self.things_processed_tasks.items()))):
-
+            callback = self.create_task_callback(task) 
+            button_title=f"{task.title} ({task.minutes} min)" if task.minutes else task.title
             menu_item = rumps.MenuItem(
-                title=f"({task.minutes} min) {task.url} {task.title}",
-                callback=self.create_time_callback(task),
-                key=str((idx + 1) % 10) if idx < 10 else ""
+                title = button_title,
+                callback = callback,
+                key = str((idx + 1) % 10) if idx < 10 else ""
             )
-            self.things_buttons[title] = menu_item
-            self.app.menu.insert_after("hours_spent", menu_item)
 
+            self.things_buttons[button_title] = menu_item
+            self.buttons_callback[button_title] = callback
+
+            self.app.menu.insert_after("hours_spent", menu_item)
 
     def run(self):
         self.app.menu[
@@ -144,24 +132,20 @@ class TimerApp(object):
         self.current_task = task
 
         for btn in [*self.things_buttons.values(), *self.buttons.values()]:
-            if sender.key == btn.key:
-                self.interval = task.minutes * SEC_TO_MIN
-                print(f"here: {self.interval}")
-                # remove time from title
-                cleaned_title = " ".join(sender.title.split()[2:])
-                self.menu_title = (
-                    " ".join(cleaned_title.split()[:4])
-                    if len(cleaned_title.split()) > 4
-                    else cleaned_title
-                )
+            if sender.title == btn.title:
                 btn.state = True
-            elif sender.title != btn.title:
+            else:
                 btn.state = False
-
-    def start_timer(self, sender, interval):
+    
+    def disable_task_items(self):
         for btn in [*self.things_buttons.values(), *self.buttons.values()]:
             btn.set_callback(None)
+    
+    def enable_task_items(self):
+        for btn in [*self.things_buttons.values(), *self.buttons.values()]:
+            btn.set_callback(self.buttons_callback[btn.title])
 
+    def on_press_start_pause(self, sender, interval):
         if sender.title.lower().startswith(("start", "continue")):
 
             if sender.title == "Start Timer":
@@ -169,20 +153,29 @@ class TimerApp(object):
                 self.timer.count = 0
                 self.timer.end = interval
 
-            setattr(self, "_taskname", getattr(self, "menu_title", ""))
             # change title of MenuItem from 'Start timer' to 'Pause timer'
             sender.title = "Pause Timer"
 
+            self.timer.stop()
             # lift off! start the timer
-            self.timer.start()
         else:  # 'Pause Timer'
             sender.title = "Continue Timer"
             self.timer.stop()
+    
+    def restart_timer(self, interval):
+        self.disable_task_items()
+
+        self.control_buttons['start_pause'].title = "Pause Timer"
+
+        self.timer.count = 0
+        self.timer.end = interval
+        self.timer.start()
 
     def on_tick(self, sender):
         time_left = sender.end - sender.count
         mins = time_left // 60 if time_left >= 0 else time_left // 60 + 1
         secs = time_left % 60 if time_left >= 0 else (-1 * time_left) % 60
+
         if mins == 0 and time_left < 0:
             self.on_last_tick(sender)
         else:
@@ -193,12 +186,19 @@ class TimerApp(object):
         sender.count += 1
 
     def on_last_tick(self, sender):
+        # TODO: add buttons?
+        #     :param action_button: title for the action button.
+        #    :param other_button: title for the other button.
+        #    :param has_reply_button: whether or not the notification has a reply button.
         rumps.notification(
                 title="Timebox",
                 subtitle="Time is up! Take a break :)",
-                message="",
+                message=self.current_task.title,
             )
-        if self.current_task.url is not '':
+            
+        os.system('afplay /System/Library/Sounds/Submarine.aiff')
+
+        if self.current_task.url != '':
             subprocess.call(
                 shlex.split("open '" + self.current_task.url + "'")
             )
@@ -207,39 +207,20 @@ class TimerApp(object):
         self.control_buttons['stop'].set_callback(None)
         self.sync_data()
 
-    def stop_timer(self, sender:rumps.MenuItem=None):
+    def stop_timer(self, sender):
+        self.enable_task_items()
 
-# TODO: add buttons?
-#     :param action_button: title for the action button.
-#    :param other_button: title for the other button.
-#    :param has_reply_button: whether or not the notification has a reply button.
-        rumps.notification(
-                title="Timebox",
-                subtitle="Time is up! Take a break :)",
-                message="",
-            )
         self.timer.stop()
         prev_count = self.timer.count - 1
         self.timer.count = 0
         self.app.title = "⏱"
         self.control_buttons['stop'].set_callback(None)
 
-        for key, btn in self.buttons.items():
-            btn.set_callback(self.buttons_callback[btn.title])
-
-        for (title, btn) in self.things_buttons.items():
-            btn.set_callback(
-                lambda _: self.set_current_task(
-                    _, self.things_processed_tasks[title]
-                )
-            )
-
-        os.system('afplay /System/Library/Sounds/Submarine.aiff')
-
-        self.sync_data()
         self.control_buttons['start_pause'].title = "Start Timer"
-        downloads_path = str(Path.home() / "Downloads" / 'log.csv')
-        with open(downloads_path, 'a+', encoding="utf-8") as f:
+        
+        self.sync_data()
+
+        with open(LOG_FILE, 'a+', encoding="utf-8") as f:
             f.write(task_to_csv(self.current_task, prev_count))
 
 # %%
